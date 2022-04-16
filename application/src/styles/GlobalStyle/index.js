d43c3e2368b468e0c16648ad33069f9affe96f95
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2022-04-16 11:59:38 -0300

    feat(GlobalStyle): adicionei um css reset padrão

--- application/src/styles/GlobalStyle/index.js
+++ application/src/styles/GlobalStyle/index.js
@@ -1,5 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
     body {
         background: rgba(40, 44, 52, 1);
         font-family: 'Roboto', sans-serif;
