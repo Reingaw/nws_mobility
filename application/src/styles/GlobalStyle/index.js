8e1604c002477c7c3eedc2e981349e4bc640612e
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2022-04-16 10:47:40 -0300

    feat: adicionei o GlobalStyle com o background e color padrão

---
+++ application/src/styles/GlobalStyle/index.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyle = createGlobalStyle`
+    body {
+        background: rgba(40, 44, 52, 1);
+        font-family: 'Roboto', sans-serif;
+        color: white;
+    }
+`;
