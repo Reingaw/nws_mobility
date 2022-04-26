984d746ca7189933db8d5ad0b3fc824b6dfbb4fd
Wagner Andrade <wagnerlinhares@bol.com.br>
2022-04-26 01:45:55 -0300

    developed header and list card wrapper

---
+++ application/src/components/list/index.jsx
@@ -0,0 +1,11 @@
+import { Card } from './style';
+
+function List() {
+  return(
+    <Card>
+      <h1>Lista</h1>
+    </Card>
+  );
+}
+
+export default List;
---
+++ application/src/components/list/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  width: 100%;
+  max-width: 500px;
+  margin: 5px auto;
+  padding: 15px;
+  background: #F6F6F6;
+  border-radius: 3px;
+  color: rgba(40,44,52,1);
+`;
