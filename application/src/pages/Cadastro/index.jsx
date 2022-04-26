26b4a7a920dfce1d73a4178ed633c711c8eab111
Wagner Andrade <wagnerlinhares@bol.com.br>
2022-04-26 01:43:04 -0300

    Code review and refactoring

--- application/src/pages/Cadastro/index.jsx
+++ application/src/pages/Cadastro/index.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import { ConteinerCadastro } from './style';
 import axios from 'axios';
 import {useForm} from 'react-hook-form';
+import { FaSearch } from 'react-icons/fa';
 
 function Cadastro () {
   const [step, setStep] = useState(1);
@@ -14,18 +15,29 @@ function Cadastro () {
     setStep(3);
   });
 
+  function searchCep() {
+    setStep(2);
+    axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(({data}) => {
+        setValue('endereço', data.logradouro);
+        setValue('complemento', data.complemento);
+        setValue('bairro', data.bairro);
+        setValue('cidade', data.localidade);
+      });
+  }
+
   switch(step) {
   case 1:
     return (
       <ConteinerCadastro>
         <div className='wrapper-progress'>
           <div className='mark'>
-            <span style={{background: 'rgba(97, 218, 251, 1)'}}></span>
+            <span className='active'></span>
             <span></span>
             <span></span>
           </div>
           <div className="progress">
-            <div className="progress-bar bg-info" role="progressbar" style={{width: '0%'}} aria-valuenow="25" aria-valuemin="25" aria-valuemax="100">
+            <div className="progress-bar" role="progressbar" style={{width: '0%'}} aria-valuenow="25" aria-valuemin="25" aria-valuemax="100">
             </div>
           </div>
         </div>
@@ -34,20 +46,9 @@ function Cadastro () {
        
         <div className="cep">
           <input type="text" className= "form-control" placeholder='Digite o CEP' value={cep} onChange={(e) => setCep(e.target.value)}/>
+          <button onClick={()=> searchCep()}><FaSearch /></button>
         </div>
-          
-        <div className='btn'>
-          <button type="button" className="btn btn-primary" onClick={() => {
-            setStep(2);
-            axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-              .then(({data}) => {
-                setValue('endereço', data.logradouro);
-                setValue('complemento', data.complemento);
-                setValue('bairro', data.bairro);
-                setValue('cidade', data.localidade);
-              });
-          }}>Proximo</button>
-        </div>
+
       </ConteinerCadastro>
     );
 
@@ -56,48 +57,47 @@ function Cadastro () {
       <ConteinerCadastro>
         <div className='wrapper-progress'>
           <div className='mark'>
-            <span style={{background: 'rgba(97, 218, 251, 1)'}}></span>
-            <span style={{background: 'rgba(97, 218, 251, 1)'}}></span>
+            <span className='active' ></span>
+            <span className='active' ></span>
             <span></span>
           </div>
           <div className="progress">
-            <div className="progress-bar bg-info" role="progressbar" style={{width: '50%'}} aria-valuenow="25" aria-valuemin="25" aria-valuemax="100">
+            <div className="progress-bar" role="progressbar" style={{width: '50%'}} aria-valuenow="25" aria-valuemin="25" aria-valuemax="100">
             </div>
           </div>
         </div>
         
               
         <form onSubmit={onSubmit}>
-          <div className="nome">
-            <input type="text" className="form-control" placeholder='Digite seu nome...' {...register('nome')}/>
+          <div className="form-control">
+            <input type="text" className="stepTwoInput" placeholder='Digite seu nome...' {...register('nome')}/>
           </div>
 
-          <div className='cidade'>
-            <input type="text" className='form-control' id='endereço' placeholder='Digite o endereço...'{...register('cidade')}/>
+          <div className='form-control'>
+            <input type="text" className='stepTwoInput' id='cidade' placeholder='Digite a cidade...'{...register('cidade')}/>
           </div>
         
-          <div className='endereço'>
-            <input type="text" className='form-control' id='endereço' placeholder='Digite o endereço...'{...register('endereço')}/>
+          <div className='form-control'>
+            <input type="text" className='stepTwoInput' id='endereço' placeholder='Digite o endereço...'{...register('endereço')}/>
           </div>
 
-          <div className="bairro">
-            <input type="text" id='numero' placeholder='Bairro' {...register('bairro')}/>
+          <div className="form-control">
+            <input type="text" className='stepTwoInput' id='numero' placeholder='Bairro' {...register('bairro')}/>
           </div>
         
-          <div className="complemento">
-            <input type="text" className="form-control" placeholder='Digite o complemento' {...register('complemento')}/>
+          <div className="form-control">
+            <input type="text" className="stepTwoInput" placeholder='Digite o complemento' {...register('complemento')}/>
           </div>
         
-          <div className='descrição'>
-            <textarea className="form-control" placeholder='Descreva o defeito...'{...register('descrição')}></textarea>
+          <div className='form-control'>
+            <textarea className="stepTwoInput" placeholder='Descreva o defeito...' {...register('descrição')}></textarea>
           </div>
 
-          <div className='btn'>
+          <div className='btn-wrapper'>
             <div>
               <button type="button" className="btn btn-primary" onClick={() => {
                 setStep(1);
               }}>Voltar</button>
-
             </div>
             <div>
               <button type="submit" className="btn btn-primary">Enviar</button>
@@ -112,12 +112,12 @@ function Cadastro () {
       <ConteinerCadastro>
         <div className='wrapper-progress'>
           <div className='mark'>
-            <span style={{background: 'rgba(97, 218, 251, 1)'}}></span>
-            <span style={{background: 'rgba(97, 218, 251, 1)'}}></span>
-            <span style={{background: 'rgba(97, 218, 251, 1)'}}></span>
+            <span className='active'></span>
+            <span className='active'></span>
+            <span className='active'></span>
           </div>
           <div className="progress">
-            <div className="progress-bar bg-info" role="progressbar" style={{width: '100%'}} aria-valuenow="25" aria-valuemin="25" aria-valuemax="100">
+            <div className="progress-bar" role="progressbar" style={{width: '100%'}} aria-valuenow="25" aria-valuemin="25" aria-valuemax="100">
             </div>
           </div>
         </div>
@@ -125,7 +125,12 @@ function Cadastro () {
         <div className='message'>
           <h4>SUCESSO</h4>
           <p>Em breve, estaremos trabalhando para<br />
-            consertar esse problema nesse endereço.</p>
+            consertar esse problema nesse endereço.
+          </p>
+          <button 
+            type="button" 
+            className="btn btn-primary" 
+            onClick={ ()=> setStep(1) }>Voltar</button>
         </div>
       </ConteinerCadastro>
     );
